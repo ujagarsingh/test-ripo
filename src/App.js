479b39bdf8f4c4d12d139aeb1e07e703e0e69bbb
gagan0425 <sainigagan3863@gmail.com>
2024-04-02 12:48:32 +0530

    install material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './Components/LoginPage';
+import Dummy from './Components/Dummy';
+import Dashboard from './Components/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <h2>kuldeep</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Dashboard />
+
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+
+   
   );
 }
 
