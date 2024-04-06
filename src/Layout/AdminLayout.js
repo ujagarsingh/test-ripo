99f346bd1759305706bc8cce2cd116ba254ba6a7
kuldeep2825 <kuldeepmina70@gmail.com>
2024-04-06 11:16:21 +0530

    home page ui

---
+++ src/Layout/AdminLayout.js
@@ -0,0 +1,86 @@
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  Menu,
+  Toolbar,
+  Typography,
+  MenuItem,
+  Avatar,
+
+} from "@mui/material";
+import * as React from "react";
+import StoreIcon from "@mui/icons-material/Store";
+import PersonIcon from "@mui/icons-material/Person";
+import { useNavigate } from "react-router-dom";
+import DashboardMenu from "../components/DashboardMenu";
+import MenuButton from "../components/MenuButton";
+
+function AdminLayout({children}) {
+
+  let navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+
+  const StyledToolBar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+
+  const handleClose = () => {
+    navigate({
+      pathname: "/",
+    });
+  };
+
+  const DashboardList = styled("div")({
+    display: "flex",
+  });
+
+  return (
+    <div className="admin_layout">
+      <AppBar direction="column" position="sticky">
+        <StyledToolBar>
+          <Typography
+            variant="h6"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            VK Enterprises
+          </Typography>
+          <StoreIcon sx={{ display: { xs: "block", sm: "none" } }} />
+          <Avatar>
+            <PersonIcon onClick={(e) => setOpen(true)}></PersonIcon>
+          </Avatar>
+        </StyledToolBar>
+
+        <Menu
+          id="demo-positioned-menu"
+          open={open}
+          onClose={(e) => setOpen(false)}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </AppBar>
+      <div className="admin_page">
+        <div className="sidebar">
+          <DashboardList>
+            <DashboardMenu />
+          </DashboardList>
+        </div>
+        <div className="content_area">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminLayout;
---
+++ src/pages/ProducrtList.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import AdminLayout from "../Layout/AdminLayout";
+
+function ProductList() {
+
+  return (
+    <AdminLayout>
+      <h2>Product List</h2>
+    </AdminLayout>
+  );
+}
+
+export default ProductList;
