db2674b9f3db50b023f5b01cac6eb49a98074ebf
gagan0425 <sainigagan3863@gmail.com>
2024-04-07 11:51:35 +0530

    bill page build

--- src/pages/Bill.js
+++ src/pages/Bill.js
@@ -1,57 +1,41 @@
 import * as React from 'react'
-import { DataGrid } from '@mui/x-data-grid';
 import AdminLayout from '../Layout/AdminLayout';
+import { Typography } from '@mui/material';
 
 const Bill = () => {
 
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+  
 
 
   return (
     <AdminLayout>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-        />
+
+      <div className='bill_container'>
+        <Typography className='bill_logo'>
+          <img src='images/favicon.png' />
+        </Typography>
+
+        <div className='bill_cover'>
+          <h1>INVOICE</h1>
+        </div>
+
+        <div className='bill_address_bar'>
+            <h2>Invoice To:</h2>
+          <div className='bill_address'>
+<h3>Invoice# <span className='invoice'>53124</span></h3>
+<h3>Date <span className='date'>01/01/2020</span></h3>
+            </div>
+            <h1>Maruf Bilah</h1>
+            <h5>25 Dummy Street Area</h5>
+            <h5>Location,Lorem Ipsum</h5>
+            <h5>9694630425</h5>
+  
+        </div>
+
+
+
       </div>
+      
     </AdminLayout>
   )
 }
